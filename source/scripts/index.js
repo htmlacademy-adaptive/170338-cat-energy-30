eb79ed2af2b91832e458156197126b1ab3b531b8
bogza.anton <bogza.anton@gmail.com>
2024-01-07 23:38:44 -0800

    Added slider for live example Added map for contacts Added no js code to be able have navigation in case of missing js file Added ability to open close main menu

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+(() => {
+  const mainNavigation = document.querySelector('.main-nav');
+  const mainNavigationToggle = document.querySelector('.main-nav__toggle');
+
+  mainNavigation.classList.remove('main-nav--no-js');
+
+  mainNavigationToggle.addEventListener('click', () => {
+    mainNavigationToggle.classList.toggle('main-nav__toggle--closed');
+    mainNavigationToggle.classList.toggle('main-nav__toggle--opened');
+  });
+})();
