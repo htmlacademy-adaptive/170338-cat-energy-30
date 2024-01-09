f2f129903e6d1bb2acca824c550032ae67986624
bogza.anton <bogza.anton@gmail.com>
2024-01-08 23:34:10 -0800

    Added interactive map

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,62 @@
 /* в этот файл добавляет скрипты*/
+let map;
+async function initMap() {
+  const position = { lat: 59.93891906738281, lng: 30.322935104370117 };
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.querySelector(".contacts__interactive-map"), {
+    zoom: 16,
+    center: position,
+    disableDefaultUI: true,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  function getMarkerSize() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      return new google.maps.Size(56, 52);
+    } else {
+      return new google.maps.Size(113, 106);
+    }
+  }
+
+  function adjustMapCenter() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1440) {
+      const newPosition = {
+        lat: position.lat,
+        lng: position.lng - 0.001
+      };
+      map.setCenter(newPosition);
+    } else {
+      map.setCenter(position);
+    }
+  }
+
+  let marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    icon: {
+      url: '../images/map-marker.png',
+      scaledSize: getMarkerSize()
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    const newSize = getMarkerSize();
+    marker.setIcon({
+      url: '../images/map-marker.png',
+      scaledSize: newSize
+    });
+
+    adjustMapCenter();
+  });
+
+  adjustMapCenter();
+}
+
 (() => {
   const mainNavigation = document.querySelector('.main-nav');
   const mainNavigationToggle = document.querySelector('.main-nav__toggle');
@@ -9,4 +67,8 @@
     mainNavigationToggle.classList.toggle('main-nav__toggle--closed');
     mainNavigationToggle.classList.toggle('main-nav__toggle--opened');
   });
+
+  window.onload = function () {
+    initMap();
+  };
 })();
